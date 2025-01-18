c60491f34ee091526fae307037bcd954072e9fe5
Af7 <discordmdp@proton.me>
2025-01-18 23:31:59 +0100

    Update db.php

--- db.php
+++ db.php
@@ -1,13 +1,13 @@
 <?php
-$host = getenv('DB_HOST');
-$dbname = getenv('DB_NAME');
-$user = getenv('DB_USER');
-$password = getenv('DB_PASSWORD');
+$host = 'mysql-messdata.alwaysdata.net'; // Adresse du serveur MySQL
+$db = 'messdata_message'; // Nom de la base de données
+$user = 'messdata'; // Utilisateur MySQL
+$pass = 'nessim2012'; // Mot de passe MySQL
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $user, $password);
+    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("Erreur de connexion à la base de données : " . $e->getMessage());
+    die("Erreur de connexion : " . $e->getMessage());
 }
 ?>
