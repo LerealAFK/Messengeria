91cf36f5a74f543baf052779558cfa8bb36b1f62
Afk7 <188451185+LerealAFK@users.noreply.github.com>
2025-03-20 11:16:38 +0100

    Update conversations.php

--- chat/conversations.php
+++ chat/conversations.php
@@ -64,7 +64,7 @@ try {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mes Conversations</title>
-    <link rel="stylesheet" href="styles/convStyle.css">
+    <link rel="stylesheet" href="../styles/convStyle.css">
 </head>
 <body>
     <div class="container">
