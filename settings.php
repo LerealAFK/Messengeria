0787ded841fb91c83872f8d90b4d83d6aa90f526
Afk7 <188451185+LerealAFK@users.noreply.github.com>
2025-03-21 10:44:45 +0100

    Update settings.php

--- settings.php
+++ settings.php
@@ -102,6 +102,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Paramètres - Messengeria</title>
     <link rel="stylesheet" href="styles/settingStyle.css">
+    <link rel="stylesheet" href="styles/notifications.css">
+    <script src="scripts/notifications.js" defer></script>
 </head>
 <body>
     <div class="container">
