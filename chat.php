7d22b6922a0240c7b9f129b2f75fd3d92e5842fd
Afk7 <188451185+LerealAFK@users.noreply.github.com>
2025-03-24 10:16:14 +0100

    Update chat.php

--- chat.php
+++ chat.php
@@ -8,11 +8,7 @@ if (!isset($_SESSION['email'])) {
     exit();
 }
 
-// Vérification que l'utilisateur appartient bien à la conversation
-if ($conversation['user1_email'] !== $user_email && $conversation['user2_email'] !== $user_email) {
-    echo "Vous ne faites pas partie de cette conversation.";
-    exit();
-}
+
 
 
 $user_email = $_SESSION['email'];
